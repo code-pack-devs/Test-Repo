2fc7629f688243209645fb026be8dc712a1691ed
Code Pack <codepack.official@gmail.com>
2021-09-01 15:29:32 +0530

    feat(about-section): about section is added.

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ app.get("/", (req, res, next) => {
   res.send("hello from Node Server");
 });
 
+app.get("/about", (req, res, next) => {
+  res.send("about myself");
+});
+
 app.listen(9000, () => {
   console.log("server is listening on 9000");
 });
