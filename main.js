b681c9655133d2c4bb2cbc8be0c4f4d9f2fe06c3
Code Pack <codepack.official@gmail.com>
2021-09-01 15:24:37 +0530

    init(workspace): worksapce has been launched.

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("hello from Node Server");
+});
+
+app.listen(9000, () => {
+  console.log("server is listening on 9000");
+});
