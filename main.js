ccefbae5d8fe0a9bfea26cbc7fdd8f38fb968c04
Amitabh <amitabh.anandcl@gmail.com>
2021-09-01 16:30:42 +0530

    for clinet-review

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ app.get("/", (req, res, next) => {
   res.send("hello from Node Server");
 });
 
+app.get("/contact", (req, res, next) => {
+  res.send("contact");
+});
+
+app.get("aboutMain", (req, res, next) => {
+  res.send("about Main");
+});
+
 app.listen(9000, () => {
   console.log("server is listening on 9000");
 });
