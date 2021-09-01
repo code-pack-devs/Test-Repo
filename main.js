5d58aa2a316bf7de5ae5f1d13e05c080bb6bd44a
Code Pack <codepack.official@gmail.com>
2021-09-01 15:24:37 +0530

    revert to older stable commit

--- main.js
+++ main.js
@@ -6,14 +6,6 @@ app.get("/", (req, res, next) => {
   res.send("hello from Node Server");
 });
 
-app.get("/contact", (req, res, next) => {
-  res.send("contact");
-});
-
-app.get("aboutMain", (req, res, next) => {
-  res.send("about Main");
-});
-
 app.listen(9000, () => {
   console.log("server is listening on 9000");
 });
